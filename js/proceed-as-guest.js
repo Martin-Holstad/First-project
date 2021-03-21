c48564a1d781a5a2961dd7fcc2055a160af3360e
Martin-Holstad <71444698+Martin-Holstad@users.noreply.github.com>
2021-03-21 14:06:06 +0100

    added js

--- js/proceed-as-guest.js
+++ js/proceed-as-guest.js
@@ -1,6 +1,113 @@
-const proceedCta = document.querySelector("#shipping-adress-guest-cta");
+const guestShippingform = document.querySelector(".shipping-adress-form");
+const guestShippingFirstNameInput = document.querySelector(
+  "#shipping-guest-first-name-input"
+);
+const guestShippingFirstNameError = document.querySelector(
+  "#shipping-adress-guest-first-name-error"
+);
+const guestShippingLastNameInput = document.querySelector(
+  "#shipping-guest-last-name-input"
+);
+const guestShippingLastNameError = document.querySelector(
+  "#shipping-adress-guest-last-name-error"
+);
+const guestShippingShippingAdressInput = document.querySelector(
+  "#shipping-guest-shipping-adress-input"
+);
+const guestShippingShippingAdressError = document.querySelector(
+  "#shipping-adress-guest-shipping-adress-error"
+);
+const guestShippingPostalCodeInput = document.querySelector(
+  "#shipping-guest-postal-code-input"
+);
+const guestShippingPostalCodeError = document.querySelector(
+  "#shipping-adress-guest-postal-code-error"
+);
+const guestShippingCountyInput = document.querySelector(
+  "#shipping-guest-county-input"
+);
+const guestShippingCountyError = document.querySelector(
+  "#shipping-adress-guest-county-error"
+);
+const guestShippingButton = document.querySelector(
+  "#shipping-adress-guest-cta"
+);
 
-proceedCta.onclick = function () {
+function buttonEnableGuestShipping() {
+  if (
+    checklengthGuestShipping(guestShippingFirstNameInput.value, 4) &&
+    checklengthGuestShipping(guestShippingLastNameInput.value, 4) &&
+    checklengthGuestShipping(guestShippingShippingAdressInput.value, 6) &&
+    checklengthGuestShipping(guestShippingPostalCodeInput.value, 4) &&
+    checklengthGuestShipping(guestShippingCountyInput.value, 4)
+  ) {
+    guestShippingButton.disabled = false;
+  } else {
+    guestShippingButton.disabled = true;
+  }
+
+  if (guestShippingFirstNameInput.value.trim().length >= 4) {
+    guestShippingFirstNameError.style.display = "none";
+  } else {
+    guestShippingFirstNameError.style.display = "block";
+  }
+
+  if (guestShippingLastNameInput.value.trim().length >= 4) {
+    guestShippingLastNameError.style.display = "none";
+  } else {
+    guestShippingLastNameError.style.display = "block";
+  }
+
+  if (guestShippingShippingAdressInput.value.trim().length >= 6) {
+    guestShippingShippingAdressError.style.display = "none";
+  } else {
+    guestShippingShippingAdressError.style.display = "block";
+  }
+
+  if (guestShippingPostalCodeInput.value.trim().length >= 4) {
+    guestShippingPostalCodeError.style.display = "none";
+  } else {
+    guestShippingPostalCodeError.style.display = "block";
+  }
+
+  if (guestShippingCountyInput.value.trim().length >= 4) {
+    guestShippingCountyError.style.display = "none";
+  } else {
+    guestShippingCountyError.style.display = "block";
+  }
+}
+
+guestShippingFirstNameInput.addEventListener(
+  "keyup",
+  buttonEnableGuestShipping
+);
+guestShippingLastNameInput.addEventListener("keyup", buttonEnableGuestShipping);
+guestShippingShippingAdressInput.addEventListener(
+  "keyup",
+  buttonEnableGuestShipping
+);
+guestShippingPostalCodeInput.addEventListener(
+  "keyup",
+  buttonEnableGuestShipping
+);
+guestShippingCountyInput.addEventListener("keyup", buttonEnableGuestShipping);
+
+function submitFormGuestShipping(event) {
+  event.preventDefault();
+  guestShippingform.reset();
+}
+
+guestShippingform.addEventListener("submit", submitFormGuestShipping);
+
+function checklengthGuestShipping(value, len) {
+  if (value.trim().length >= len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+guestShippingButton.onclick = function () {
   location.href =
     "https://mh-rainydays.netlify.app/purchase/choose-payment-method.html";
 };
--- js/proceed-with-register.js
+++ js/proceed-with-register.js
@@ -1,3 +1,83 @@
 const formRegisterShipping = document.querySelector(
   ".register-shipping-adress-form"
 );
+const registerShippingAdressInput = document.querySelector(
+  "#register-shipping-adress-input"
+);
+const registerShippingAdressError = document.querySelector(
+  "#register-shipping-adress-error-display"
+);
+const registerShippingPostalInput = document.querySelector(
+  "#register-shipping-postal-input"
+);
+const registerShippingPostalError = document.querySelector(
+  "#register-shipping-postal-error-display"
+);
+const registerShippingCountyInput = document.querySelector(
+  "#register-shipping-county-input"
+);
+const registerShippingCountyError = document.querySelector(
+  "#register-shipping-county-error-display"
+);
+const registerShippingAdressProceedButton = document.querySelector(
+  ".cta-register-shipping-adress"
+);
+
+function buttonEnableRegisterShipping() {
+  if (
+    checklengthRegisterShipping(registerShippingAdressInput.value, 6) &&
+    checklengthRegisterShipping(registerShippingPostalInput.value, 4) &&
+    checklengthRegisterShipping(registerShippingCountyInput.value, 4)
+  ) {
+    registerShippingAdressProceedButton.disabled = false;
+  } else {
+    registerShippingAdressProceedButton.disabled = true;
+  }
+
+  if (registerShippingAdressInput.value.trim().length >= 6) {
+    registerShippingAdressError.style.display = "none";
+  } else {
+    registerShippingAdressError.style.display = "block";
+  }
+
+  if (registerShippingPostalInput.value.trim().length >= 4) {
+    registerShippingPostalError.style.display = "none";
+  } else {
+    registerShippingPostalError.style.display = "block";
+  }
+
+  if (registerShippingCountyInput.value.trim().length >= 4) {
+    registerShippingCountyError.style.display = "none";
+  } else {
+    registerShippingCountyError.style.display = "block";
+  }
+}
+
+registerShippingAdressInput.addEventListener(
+  "keyup",
+  buttonEnableRegisterShipping
+);
+registerShippingPostalInput.addEventListener(
+  "keyup",
+  buttonEnableRegisterShipping
+);
+registerShippingCountyInput.addEventListener(
+  "keyup",
+  buttonEnableRegisterShipping
+);
+
+function submitFormRegisterShipping(event) {
+  event.preventDefault();
+
+  formRegisterShipping.reset();
+}
+
+formRegisterShipping.addEventListener("submit", submitFormRegisterShipping);
+
+function checklengthRegisterShipping(value, len) {
+  if (value.trim().length >= len) {
+    return true;
+  } else {
+    return false;
+  }
+}
